63e57ab9fdd6e546ea806eb8f9cf5eeac2b707af
lauralvd01 <laura.levraud1@etu.univ-lorraine.fr>
2025-01-14 07:55:15 +0000

    Display only categories with modules effectively connected : OK. Reactive display of connected modules OK !!!!!!!!!!! To do now : incorporate it in the main page

--- src/routes/user_manual/+page.svelte
+++ src/routes/user_manual/+page.svelte
@@ -2,7 +2,7 @@
     import { onMount } from "svelte";
     import { writable } from "svelte/store";
 
-    const connected_modules = writable([])
+    const connected_modules = writable([]);
 
     async function fetchConnectedModules() {
         try {
@@ -15,17 +15,17 @@
         }
     }
 
-    let modules_by_categories = {"Stockage d'énergie": {}, "Mobilité": {}, "Processeur": {}, "Réseau": {}, "Production d'énergie": {}, "Capteur": {}}
+    const modules_by_categories = writable({"Stockage d'énergie": {}, "Mobilité": {}, "Processeur": {}, "Réseau": {}, "Production d'énergie": {}, "Capteur": {}});
     connected_modules.subscribe((connected_modules_value) => {
         // Group modules by functionality
-        let modules_group_by_categories = Object.groupBy(connected_modules_value, ({funcionality}) => funcionality);
-
+        let modules_group_by_categories = Object.groupBy(connected_modules_value, ({functionality}) => functionality);
+        
         Object.entries(modules_group_by_categories).forEach(element => {
             let [functionality, modules] = element;
             // For each functionality, count and store occurences of each module name : { name_module1: count_module1, name_module2: count_module2, ... }
-            modules_by_categories[functionality] = modules.map(({name}) => name).reduce(((acc, val) => { acc[val] = ( acc[val] || 0) + 1; return acc}), {});
+            $modules_by_categories[functionality] = modules.map(({name}) => name).reduce(((acc, val) => { acc[val] = ( acc[val] || 0) + 1; return acc}), {});
         });
-    })
+    });
 
     onMount(async () => {
         await fetchConnectedModules();
@@ -43,24 +43,21 @@
     <h1 class="titles">EQUIPEMENTS ROBOT</h1>
     <div class="scrolable">
         <div>Module test</div>
-        {#each Object.entries(modules_by_categories) as [funcionality, modules]}
-            {#if funcionality == "Stockage d'énergie"}
-                <Storage modules={modules}/>
-            {/if}
-            {#if funcionality == "Mobilité"}
-                <Mobility modules={modules}/>
-            {/if}
-            {#if funcionality == "Processeur"}
-                <Processor modules={modules}/>
-            {/if}
-            {#if funcionality == "Réseau"}
-                <Network modules={modules}/>
-            {/if}
-            {#if funcionality == "Production d'énergie"}
-                <Production modules={modules}/>
-            {/if}
-            {#if funcionality == "Capteur"}
-                <Sensor modules={modules}/>
+        {#each Object.entries($modules_by_categories) as [functionality, modules]}
+            {#if Object.entries(modules).length > 0}
+                {#if functionality == "Stockage d'énergie"}
+                    <Storage modules={modules}/>
+                {:else if functionality == "Mobilité"}
+                    <Mobility modules={modules}/>
+                {:else if functionality == "Processeur"}
+                    <Processor modules={modules}/>
+                {:else if functionality == "Réseau"}
+                    <Network modules={modules}/>
+                {:else if functionality == "Production d'énergie"}
+                    <Production modules={modules}/>
+                {:else if functionality == "Capteur"}
+                    <Sensor modules={modules}/>
+                {/if}
             {/if}
         {/each}
     </div>
